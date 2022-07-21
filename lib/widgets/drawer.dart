28bc88a8d04e0b57b543f176b166d35b755f13f2
Maya Shankar Jha <mayashankarjha301@gmail.com>
2022-07-21 20:54:02 +0530

    day 8 completed

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore_for_file: prefer_const_constructors
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageURL =
+        "https://pbs.twimg.com/profile_images/1277591126065569795/1L1Ymlkd_400x400.jpg";
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                accountEmail: Text("maya@gmail.com"),
+                accountName: Text("Maya Shankar Jha"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageURL),
+                ),
+              )),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.home,
+            ),
+            title: Text(
+              "Home",
+              textScaleFactor: 1.2,
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.profile_circled,
+            ),
+            title: Text(
+              "Profile",
+              textScaleFactor: 1.2,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
